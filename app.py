394b7a3b019ccc841e7dfe97cbf29e887f305172
Paras Modi <parasm346@gmail.com>
2014-04-05 18:17:20 -0400

    more facebook stuff

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect
 import jinja2
 import os
 import facebook
+import pymongo
+import sendgrid
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -17,7 +20,10 @@ def user():
 	except AttributeError, e:
 		return redirect('/')
 	graph = facebook.GraphAPI(token)
-	print graph
+	profile = graph.get_object("me")
+	me = profile.get('first_name') +" "+ profile.get('last_name')
+	print me
+	return "worked"
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 8000))
 	app.run(host='0.0.0.0', port=port,debug=True)
