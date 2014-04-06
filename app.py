45dc4ca962cf8f9fa6aff2af67e328096620384e
Paras Modi <parasm346@gmail.com>
2014-04-06 09:52:27 -0400

    views

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ def plain():
 def inserty():
 	if request.method == "POST":
 		user = request.form.get('user')
-		insertall(user)
+		print user
 		return redirect('/')
 @app.route('/extensions', methods=['GET','POST'])
 def extend():
