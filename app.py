aabab1753c5697584e6c82602ea12c4eb41cfa95
Paras Modi <parasm346@gmail.com>
2014-04-06 11:10:53 -0400

    new db

--- app.py
+++ app.py
@@ -164,11 +164,11 @@ def facebook_id(id):
 	flash = []		
 	for p in members:
 		image_url ='https://graph.facebook.com/'+p.get('id')+'/picture'
-		flash.append({'question':{'type':'image','value':image_url},'answer':{'type':'text','value':p.get('name')},'attempts':0,'stage':0,'time':int(time.time())})
+		flash.append({'question':{'type':'image','value':image_url},'answer':{'type':'text','value':p.get('name')},'attempts':0,'stage':0})
 	id = flashcards.insert({'username':profile.get('username'),'cards':flash,'deck_name':group.get('name'),
 		'reminded':True,'fb_id':profile.get('id')})
 	person = users.find({'fb_id':profile.get('id')}).limit(1)[0]
-	person['flashcards'].append({'time':time.time(),'username':profile.get('username'),'cards':flash,
+	person['flashcards'].append({'time':int(time.time()),'username':profile.get('username'),'cards':flash,
 		'deck_name':group.get('name'),'fb_id':profile.get('id')})
 	users.update({'fb_id':profile.get('id')},person)
 	return redirect('/decks')
