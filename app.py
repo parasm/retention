2237cb758e293cbaad199f85d9815ff76e998533
Paras Modi <parasm346@gmail.com>
2014-04-05 22:37:45 -0400

    user login

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 import sendgrid
 from bson.objectid import ObjectId
 
-client = MongoClient("mongodb://admin:pretzelssux201@oceanic.mongohq.com:10099/retention")
+client = MongoClient("mongodb://admin: @oceanic.mongohq.com:10099/retention")
 db = client.get_default_database()
 users = db.users
 
@@ -18,22 +18,24 @@ def hello():
 	return render_template("index.html")
 @app.route('/user')
 def user():
-	# user = facebook.get_user_from_cookie(request.cookies,'1420579554861962','b1ffcae9099845e428322a88193bc072')
-	# try:
-	# 	token = user.get('access_token')
-	# 	print token
-	# except AttributeError, e:
-	# 	return redirect('/')
-	token = "CAAUMAnOZBI4oBAFh6a3dZCZAh9VgremW7nTmu5Cnak4GsvmGtRuUrJhTQuqN7CY114umg2eJNMeTjHDQuTlHYPGib0piCLZCHYpd0Si1w5PCzi54ZBJjVWemExwOsD2MoRxik8YQxpqlA7VjlP0CutnfMqYKsGoB0n6y4ZCqZC9oWfzgGhgnlYZCDg2yZBSsrhnsZD"
+	user = facebook.get_user_from_cookie(request.cookies,'1420579554861962','b1ffcae9099845e428322a88193bc072')
+	try:
+		token = user.get('access_token')
+		print token
+	except AttributeError, e:
+		return redirect('/')
 	graph = facebook.GraphAPI(token)
 	profile = graph.get_object("me")
 	me = profile.get('first_name') +" "+ profile.get('last_name')
 	friends = graph.get_connections("me", "friends").get('data')
 	groups = graph.get_connections("me","groups").get('data')
+	all_people = []
+	gs = []
 	for g in groups:
 		#print graph.get_object(g.get('id'))
-		print graph.get_connections(g.get('id'),"members")
-	return "groups"
+		gs.append(g.get('id'))
+		all_people.append(graph.get_connections(g.get('id'),"members"))
+	return render_template('list.html',groups=gs, people=all_people,count=len(groups))
 @app.route('/token',methods=['GET','POST'])
 def get_token():
 	if request.method == "POST":
