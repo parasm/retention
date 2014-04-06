e6d85baa098ec4d0554082136771293a97f56122
Paras Modi <parasm346@gmail.com>
2014-04-05 20:52:51 -0400

    groups names + people

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template, request, redirect
 import jinja2
 import os
 import facebook
-import pymongo
+from pymongo import MongoClient
 import sendgrid
 from bson.objectid import ObjectId
+
+client = MongoClient("mongodb://admin:pretzelssux201@oceanic.mongohq.com:10099/retention")
+db = client.get_default_database()
+users = db.users
+
 
 app = Flask(__name__)
 
@@ -13,20 +18,22 @@ def hello():
 	return render_template("index.html")
 @app.route('/user')
 def user():
-	user = facebook.get_user_from_cookie(request.cookies,'1420579554861962','b1ffcae9099845e428322a88193bc072')
-	print user
-	try:
-		token = user.get('access_token')
-	except AttributeError, e:
-		return redirect('/')
+	# user = facebook.get_user_from_cookie(request.cookies,'1420579554861962','b1ffcae9099845e428322a88193bc072')
+	# try:
+	# 	token = user.get('access_token')
+	# 	print token
+	# except AttributeError, e:
+	# 	return redirect('/')
+	token = "CAAUMAnOZBI4oBAFh6a3dZCZAh9VgremW7nTmu5Cnak4GsvmGtRuUrJhTQuqN7CY114umg2eJNMeTjHDQuTlHYPGib0piCLZCHYpd0Si1w5PCzi54ZBJjVWemExwOsD2MoRxik8YQxpqlA7VjlP0CutnfMqYKsGoB0n6y4ZCqZC9oWfzgGhgnlYZCDg2yZBSsrhnsZD"
 	graph = facebook.GraphAPI(token)
 	profile = graph.get_object("me")
 	me = profile.get('first_name') +" "+ profile.get('last_name')
-	print dir(graph)
 	friends = graph.get_connections("me", "friends").get('data')
 	groups = graph.get_connections("me","groups").get('data')
-	print groups
-	return "friends"
+	for g in groups:
+		#print graph.get_object(g.get('id'))
+		print graph.get_connections(g.get('id'),"members")
+	return "groups"
 @app.route('/token',methods=['GET','POST'])
 def get_token():
 	if request.method == "POST":
